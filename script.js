71e8130c4010f889b4c9f0376ad7fef10df02699
Ryan <rmurph1800@gmail.com>
2021-09-01 15:38:53 -0400

    wrote js file

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
 
+$(document).ready(function()  {
+    // display current day/time using moment js
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    // saves schedule text and time to local storage
+    $(".saveBtn").on("click", function() {
+
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
 
 
+// schedule text and time pulled from local storage (one for each created hour)
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour18 .description").val(localStorage.getItem("hour18"));
+$("#hour19 .description").val(localStorage.getItem("hour19"));
+$("#hour20 .description").val(localStorage.getItem("hour20"));
 
 
+function hours() {
+    // finds current hour
+    var currentHour = moment().hour();
+    // determines what class to apply depending on current hour 
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
 
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
+
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+}
+    hours();
+
+})
